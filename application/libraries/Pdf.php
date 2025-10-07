d14a1680d05d1296c8591c0d50e879252e1a1094
tanmoym483 <tanmoym483@gmail.com>
2025-10-07 13:00:40 +0530

    changes in pdf.php

--- application/libraries/Pdf.php
+++ application/libraries/Pdf.php
@@ -1,51 +1,62 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
+
 /**
  * CodeIgniter DomPDF Library
  *
  * Generate PDF's from HTML in CodeIgniter
  *
- * @packge        CodeIgniter
- * @subpackage        Libraries
- * @category        Libraries
- * @author        Ardianta Pargo
+ * @package        CodeIgniter
+ * @subpackage     Libraries
+ * @category       Libraries
+ * @author         Ardianta Pargo
  * @license        MIT License
- * @link        https://github.com/ardianta/codeigniter-dompdf
+ * @link           https://github.com/ardianta/codeigniter-dompdf
  */
+
 use Dompdf\Dompdf;
-class Pdf extends Dompdf{
+use Dompdf\Options;
+
+class Pdf extends Dompdf {
     /**
      * PDF filename
      * @var String
      */
     public $filename;
-    public function __construct(){
-        parent::__construct();
+
+     public function __construct() {
+        // Set DomPDF options
+        $options = new Options();
+        $options->set('isRemoteEnabled', true); // allow file:// and external URLs
+        $options->set('defaultFont', 'Helvetica'); // optional default font
+
+        // Pass options to Dompdf constructor
+        parent::__construct($options);
+
         $this->filename = "laporan.pdf";
     }
+
     /**
      * Get an instance of CodeIgniter
      *
-     * @access    protected
-     * @return    void
+     * @access protected
+     * @return object
      */
-    protected function ci()
-    {
+    protected function ci() {
         return get_instance();
     }
+
     /**
      * Load a CodeIgniter view into domPDF
      *
-     * @access    public
-     * @param    string    $view The view to load
-     * @param    array    $data The view data
-     * @return    void
+     * @access public
+     * @param  string $view  The view to load
+     * @param  array  $data  The view data
+     * @return void
      */
-    public function load_view($view, $data = array()){
-        $html = $this->ci()->load->view($view, $data, TRUE);
-        $this->load_html($html);
-        // Render the PDF
+    public function load_view($view, $data = array()) {
+        $html = $this->ci()->load->view($view, $data, true);
+        $this->loadHtml($html);
         $this->render();
-            // Output the generated PDF to Browser
-               $this->stream($this->filename, array("Attachment" => false));
+        $this->stream($this->filename, array("Attachment" => false)); // false = inline preview
     }
 }
